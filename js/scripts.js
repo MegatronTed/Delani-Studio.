f1f9de2dc8662096f11138bf3c0299f4df4f95b3
MegatronTed <santa@northpole.com>
2019-09-30 16:39:59 +0300

    addition of content on both style.css and index.html

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(documen).ready(function(){
+    $(".main").click(function(){
+        $("#design").toggle();
+        $("#space").toggle();
+    });
+    $(".mai").click(function(){
+        $("#development").toggle();
+        $("#road").toggle();
+    });
+    $(".kim").click(function(){
+        $("#product").toggle();
+        $("#craft").toggle();
+    });
+});
